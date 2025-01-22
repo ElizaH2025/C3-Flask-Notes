87c03510f0a26964312ce251e013d68b60af053e
ElizaH2025 <eliza.horowitz2025@gbwl.org>
2025-01-22 15:41:11 +0000

    yay

--- app.py
+++ app.py
@@ -8,11 +8,26 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+# New functions
+@app.route("/about/")
+def about():
+     return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+     return render_template("contact.html")
+
 # new route
 @app.route("/hello/")
 @app.route("/hello/<name_data>")
 def hello_there(name_data = None):
     return render_template("hello_there.html", name = name_data)
+
+@app.route("/api/data")
+def get_data():
+    # displays data file returned from an API call
+    # can do more python work to format this later
+    return app.send_static_file("data.json")
 
 # Allows you to click "Run" button
 if __name__ == "__main__":
